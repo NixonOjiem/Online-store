04ce1ee446e03a841d0f7eae876d11d092cc81c9
arkstudiodevelopers <nicksonogm@gmail.com>
2024-09-24 08:28:28 +0300

    With a dammy heading

---
+++ src/components/Header.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <div className='My-Header'>
+        <p className='Icon'>Icon</p>
+            
+    </div>
+  )
+}
+
+export default Header
