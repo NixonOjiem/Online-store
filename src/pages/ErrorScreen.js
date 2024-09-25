2e16d9e6c33a2d911a09da0cb0bf46009daebd09
arkstudiodevelopers <nicksonogm@gmail.com>
2024-09-25 10:42:46 +0300

    commit secure

--- src/pages/ErrorScreen.js
+++ src/pages/ErrorScreen.js
@@ -5,7 +5,7 @@ import Navigation from '../components/Navigation';
 function ErrorScreen() {
   return (
     <div>
-        <Navigation />
+        {/* <Navigation /> */}
         <div className='Error'>
            <h1>Page 404!</h1>  <br />
            <p>Sorry, the page you are looking for does not exist. click here to go to <Link to='/home'>Home</Link></p>
