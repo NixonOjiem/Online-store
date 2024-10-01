96c00a4ba2147488cd3b8ccf2d5b6154d79c7bc6
arkstudiodevelopers <nicksonogm@gmail.com>
2024-10-01 18:31:57 +0300

    Pop-up messages

--- src/components/MousePics.js
+++ src/components/MousePics.js
@@ -12,19 +12,76 @@ import mouse9 from '../images/mouse9.jpg';
 
 
 function MousePics() {
+
+  function handleClick(image, name){
+    // Create a popup element
+    const popup = document.createElement('div');
+    popup.className = 'popup';
+
+    // Create a content element for the popup
+    const content = document.createElement('div');
+    content.className = 'popup-content';
+
+    // Add the image and name to the content element
+    content.innerHTML = `
+    <img src=${image} />
+    <h2>${name}</h2>
+    <p>More details about ${name}...</p>
+    `;
+
+    // Add the content element to the popup element
+    popup.appendChild(content);
+
+    // Add the popup element to the body
+    document.body.appendChild(popup);
+  }
+  
+  document.addEventListener('click', (e) => {
+    if (e.target.className === 'popup') {
+      e.target.remove();
+    }
+  });
+
   return (
     <div className='mouse-container'>
         <h4 className='Category-Name'>Top Selling mouse</h4>
         <div className='mouse-pic-row'>
-            {/* <img src= {mouse1} className='mouse-pics'/> */}
-            <img src= {mouse2} className='mouse-pics'/>
-            <img src= {mouse3} className='mouse-pics'/>
-            <img src= {mouse4} className='mouse-pics'/>
-            <img src= {mouse5} className='mouse-pics'/>
-            <img src= {mouse6} className='mouse-pics'/>
-            <img src= {mouse7} className='mouse-pics'/>
-            <img src= {mouse8} className='mouse-pics'/>
-            <img src= {mouse9} className='mouse-pics'/>
+          <div className='mouse-pic'>
+            <img src= {mouse1} className='mouse-pics' onClick={() => handleClick(mouse1, 'Custom U16')}/>
+            <p>Custom U16</p>
+          </div>
+          <div className='mouse-pic'>
+            <img src= {mouse2} className='mouse-pics' onClick={() => handleClick(mouse2, 'Custom H15')}/>
+            <p>Custom H15</p>
+          </div>
+          <div className='mouse-pic'>
+            <img src= {mouse3} className='mouse-pics' onClick={() => handleClick(mouse3, 'Custom P14')}/>
+            <p>Custom P14</p>
+          </div>
+          <div className='mouse-pic'>
+            <img src= {mouse4} className='mouse-pics' onClick={() => handleClick(mouse4, 'Custom M15')}/>
+            <p>Custom M15</p>
+          </div>
+          <div className='mouse-pic'>
+            <img src= {mouse5} className='mouse-pics' onClick={() => handleClick(mouse5, 'Custom N15')}/>
+            <p>Custom N15</p>
+          </div>
+          <div className='mouse-pic'>
+            <img src= {mouse6} className='mouse-pics' onClick={() => handleClick(mouse6, 'Custom R15')}/>
+            <p>Custom R15</p>
+          </div>
+          <div className='mouse-pic'>
+            <img src= {mouse7} className='mouse-pics' onClick={() => handleClick(mouse7, 'Custom X15')}/>
+            <p>Custom X15</p>
+          </div>
+          <div className='mouse-pic'>
+            <img src= {mouse8} className='mouse-pics' onClick={() => handleClick(mouse8, 'Custom Y15')}/>
+            <p>Custom Y15</p>
+          </div>
+          <div className='mouse-pic'>
+            <img src= {mouse9} className='mouse-pics' onClick={() => handleClick(mouse9, 'Custom Z15')}/>
+            <p>Custom Z15</p>
+          </div>
         </div>
     </div>
   )
